c794289cf6f6cbb01f4efc7ce849aed302432bcd
Hans Meyer <hansifer@users.noreply.github.com>
2022-06-16 16:45:39 -0600

    add multi props

---
+++ src/Locations.js
@@ -0,0 +1,64 @@
+import {
+  Paper,
+  Container,
+  Grid,
+  Button,
+  FormGroup,
+  FormControlLabel,
+  Switch,
+  Box,
+} from '@mui/material';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import RoomIcon from '@mui/icons-material/Room';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+export function Locations({ locations, isMulti, onClear, onChangeMulti }) {
+  return (
+    <Paper sx={{ padding: 2 }}>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <Typography variant="h6" component="div">
+            Clicked locations
+          </Typography>
+        </Grid>
+        <Grid item container xs justifyContent="flex-end" alignItems="center">
+          <FormControlLabel
+            control={<Switch checked={isMulti} onChange={onChangeMulti} />}
+            label="Multi"
+          />
+          <Button variant="contained" size="small" onClick={onClear} disabled={!locations.length}>
+            Clear
+          </Button>
+        </Grid>
+      </Grid>
+      {!locations?.length ? (
+        <Box
+          sx={{
+            display: 'flex',
+            marginTop: 2,
+            justifyContent: 'center',
+            color: '#aaa',
+          }}
+        >
+          none
+        </Box>
+      ) : (
+        <List sx={{ width: '100%' }} dense={true}>
+          {locations.map(location => (
+            <ListItem>
+              <ListItemIcon>
+                <RoomIcon />
+              </ListItemIcon>
+              <ListItemText primary={location} />
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </Paper>
+  );
+}
