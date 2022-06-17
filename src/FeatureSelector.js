efd8f6b73ab96651a2e72b62c865ebc81503a55e
Hans Meyer <hansifer@users.noreply.github.com>
2022-06-16 19:27:54 -0600

    add FeatureSelector comp

---
+++ src/FeatureSelector.js
@@ -0,0 +1,26 @@
+import { List, ListItem, ListItemButton, ListItemText, Checkbox } from '@mui/material';
+
+export const FeatureSelector = ({ features, onFeatureToggled }) => {
+  return (
+    <List dense sx={{ width: 180, maxWidth: 180 }}>
+      {features.map(feature => (
+        <ListItem
+          key={feature.id}
+          secondaryAction={
+            <Checkbox
+              edge="end"
+              onChange={e => onFeatureToggled(feature.id, e.target.checked)}
+              checked={feature.selected}
+              inputProps={{ 'aria-labelledby': `checkbox-list-secondary-label-${feature.id}` }}
+            />
+          }
+          disablePadding
+        >
+          <ListItemButton>
+            <ListItemText id={feature.id} primary={feature.label} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
