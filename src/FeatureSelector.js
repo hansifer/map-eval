2b480da8ae8d6580f0d043b3ea6576977d395a22
Hans Meyer <hansifer@users.noreply.github.com>
2022-06-20 13:40:07 -0600

    add header

--- src/FeatureSelector.js
+++ src/FeatureSelector.js
@@ -4,43 +4,53 @@ import {
   ListItemButton,
   ListItemText,
   Checkbox,
+  Typography,
+  Paper,
 } from '@mui/material';
 
 export const FeatureSelector = ({ features, onFeatureToggled }) => {
   return (
-    <List
-      dense
-      sx={{ width: 180, maxWidth: 180 }}
-    >
-      {features.map((feature) => (
-        <ListItem
-          key={feature.id}
-          secondaryAction={
-            <Checkbox
-              disabled={feature.disabled}
-              edge="end"
-              onChange={() => onFeatureToggled(feature.id)}
-              checked={feature.selected ?? false} // prevent `undefined` to let MUI know this checkbox is controlled
-              inputProps={{
-                'aria-labelledby': `checkbox-list-secondary-label-${feature.id}`,
-              }}
-            />
-          }
-          disablePadding
-        >
-          <ListItemButton
-            disabled={feature.disabled}
-            onClick={() => {
-              onFeatureToggled(feature.id);
-            }}
+    <Paper sx={{ padding: 2 }}>
+      <Typography
+        variant="h6"
+        component="div"
+      >
+        Layers
+      </Typography>
+      <List
+        dense
+        sx={{ width: 180, maxWidth: 180 }}
+      >
+        {features.map((feature) => (
+          <ListItem
+            key={feature.id}
+            secondaryAction={
+              <Checkbox
+                disabled={feature.disabled}
+                edge="end"
+                onChange={() => onFeatureToggled(feature.id)}
+                checked={feature.selected ?? false} // prevent `undefined` to let MUI know this checkbox is controlled
+                inputProps={{
+                  'aria-labelledby': `checkbox-list-secondary-label-${feature.id}`,
+                }}
+              />
+            }
+            disablePadding
           >
-            <ListItemText
-              id={feature.id}
-              primary={feature.label}
-            />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
+            <ListItemButton
+              disabled={feature.disabled}
+              onClick={() => {
+                onFeatureToggled(feature.id);
+              }}
+            >
+              <ListItemText
+                id={feature.id}
+                primary={feature.label}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Paper>
   );
 };
