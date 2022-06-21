afaa410e5c05df559aacfe76686d75654c533db9
Hans Meyer <hansifer@users.noreply.github.com>
2022-06-20 18:54:57 -0600

    add HERE util

---
+++ src/utils/vendor/HERE.js
@@ -0,0 +1,24 @@
+import { getMarkerSVG } from '/utils/marker';
+
+// return object should implement H.clustering.ITheme interface
+export const getCustomClusteringTheme = (defaultTheme) => ({
+  getClusterPresentation: (cluster) =>
+    defaultTheme.getClusterPresentation.call(defaultTheme, cluster), // keep default theme for clusters
+  getNoisePresentation: (noisePoint) => {
+    const H = window.H;
+
+    const location = noisePoint.getData();
+    const icon = new H.map.Icon(getMarkerSVG(location.color));
+
+    const noiseMarker = new H.map.Marker(noisePoint.getPosition(), {
+      // use min zoom from a noise point to show it correctly at certain zoom levels
+      min: noisePoint.getMinZoom(),
+      icon,
+    });
+
+    // link a data from the point to the marker to make it accessible inside onMarkerClick
+    noiseMarker.setData(location);
+
+    return noiseMarker;
+  },
+});
